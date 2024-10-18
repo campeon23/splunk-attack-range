18a9eb2c0f3b5d0defedd2b0d78e3eb355150ff0
Marco Villarruel <marco@guilda.io>
2024-10-18 13:16:51 -0600

    Initial commit: Clone of Splunk Attack Range
    - Added all files and configurations from the Splunk Attack Range repository clone. This includes infrastructure deployment scripts, Ansible playbooks, Python automation, and security tool integration support for AWS and Azure.

---
+++ terraform/aws/modules/windows/variables.tf
@@ -0,0 +1,10 @@
+
+variable "vpc_security_group_ids" { }
+variable "ec2_subnet_id" { }
+variable "general" { }
+variable "aws" { }
+variable "windows_servers" { }
+variable "splunk_server" { }
+variable "simulation" { }
+variable "zeek_server" { }
+variable "snort_server" { }
---
+++ terraform/azure/modules/splunk-server/variable.tf
@@ -0,0 +1,13 @@
+
+variable "rg_name" { }
+variable "subnet_id" { }
+variable "azure" { }
+variable "splunk_server" { }
+variable "phantom_server" { }
+variable "general" { }
+variable "simulation" { }
+variable "windows_servers" { }
+variable "linux_servers" { }
+variable "zeek_server" { }
+variable "kali_server" { }
+variable "snort_server" { }
